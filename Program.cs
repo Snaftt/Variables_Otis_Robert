7d381b9d3c6edea1bc5dc5d916d4691eff6974fb
Good morning yawl <robertc.otis791@gmail.com>
2024-02-22 08:25:31 -0600

    Variables_Robert.Otis
    yes

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿{
+    int favoriteNumber; // Declares an integer to store a favorite number
+    bool isJumping = false, isRunning = false; //Booleans initaialized to false for jumping and running
+    float myFloat;// Random flote variable
+    favoriteNumber = 14; // makes your favoriteNumber to 6
+    myFloat = 89.18; // Assigning the myFloat to 89.18
+    const double funalGrade = 90.0; // Initializing constant finalgrade to 90
+
+
+        // prents to the console
+        Console.WriteLine("Favorite #:" + favoriteNumber);
+        Console.WriteLine("jumping: " + isJumping);
+        Console.WriteLine("Running: " + isRunning);
+        Console.WriteLine("Random Floating #: " + myFloat);
+        Console.WriteLine("Final Grade: " + funalGrade);
+
+
+
+
+
+
+
+
+}
